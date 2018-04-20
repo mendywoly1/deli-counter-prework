382a500e37c8b8784a7a6cee0c4c92b7c65ecb0a
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 02:15:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{array.s} in line."
+  puts "Welcome, #{name}. You are number #{array.index(name)} in line."
 end
   
   
