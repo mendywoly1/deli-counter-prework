68c57a04aed4bb46400080494bbf63ecc03e9d31
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-20 02:07:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,9 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   end
+  line = "The line is currently: "
+  array.each_with_index do |val, index|
+    line << "#{index + 1}. #{val}"
+    
   
 end
